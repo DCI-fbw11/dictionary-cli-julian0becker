108cda1bd1a93e01de83d327e5780bf5f7fac380
julian0becker <42543792+julian0becker@users.noreply.github.com>
2019-03-19 19:46:39 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
+const axios = require('axios');
+const fs = require('fs');
+
 const main = () => {
-  const [, , ...args] = process.argv
+  const [, , ...args] = process.argv;
   if (args[0]) {
-    console.log("Ciao!")
+    fetchData(args[0]);
   }
-  console.log(process.argv)
+};
+
+async function fetchData(entry) {
+  const response = await axios.get(
+    `https://od-api.oxforddictionaries.com:443/api/v1/entries/en/${entry}`,
+    {
+      headers: {
+        Accept: 'application/json',
+        app_id: '40ea3c70',
+        app_key: 'a6da97671faf29ac523fca72aa29d87d'
+      }
+    }
+  );
+  const { results } = response.data;
+
+  const terms = results[0].lexicalEntries.map(entry => entry.text);
+  const categories = results[0].lexicalEntries.map(
+    entry => entry.lexicalCategory
+  );
+  const definitions = results[0].lexicalEntries.map(
+    entry => entry.entries[0].senses[0].short_definitions[0]
+  );
+
+  let temporary = [];
+  for (let i = 0; i < terms.length; i++) {
+    temporary.push([terms[i], categories[i], definitions[i]]);
+  }
+
+  let message = temporary.map(
+    entry => `\n${entry[0]} (${entry[1]}): ${entry[2]}`
+  );
+
+  message.push('\nProvided by Oxford University Press');
+
+  fs.writeFile(`./${entry}.txt`, message, err => {
+    if (err) {
+      console.log('there as an error');
+    }
+  });
 }
 
-module.exports = main
+// module.exports = main;
+main();
